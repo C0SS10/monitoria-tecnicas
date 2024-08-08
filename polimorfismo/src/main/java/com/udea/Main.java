2972c8dce16ed913ec5fcb35fed204ba54de7c92
Esteban <estebancg12@hotmail.com>
2024-08-07 20:28:24 -0500

    🧰 Creando varias propiedades

--- polimorfismo/src/main/java/com/udea/Main.java
+++ polimorfismo/src/main/java/com/udea/Main.java
@@ -1,7 +1,21 @@
 package com.udea;
 
+import com.udea.classes.Casa;
+import com.udea.classes.Departamento;
+import com.udea.classes.LocalComercial;
+import com.udea.classes.Propiedad;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Propiedad[] propiedades = new Propiedad[3];
+        propiedades[0] = new LocalComercial("Calle 1 # 2-3", 100, true);
+        propiedades[1] = new Departamento("Calle 2 # 3-4", 200, 3, 2);
+        propiedades[2] = new Casa("Calle 3 # 4-5", 300, 50);
+
+        for (Propiedad propiedad : propiedades) {
+            System.out.println(propiedad.mostrarDescripcion());
+            System.out.println("Precio de alquiler: $" + propiedad.calcularPrecioAlquiler());
+            System.out.println();
+        }
     }
 }
