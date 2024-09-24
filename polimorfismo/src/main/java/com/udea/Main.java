ea9138ab1d5aef0aa1943acf317d7d8a4874fc13
Esteban <estebancg12@hotmail.com>
2024-09-24 15:49:49 -0500

    Limpiando proyecto 🧹

--- polimorfismo/src/main/java/com/udea/Main.java
+++ polimorfismo/src/main/java/com/udea/Main.java
@@ -1,21 +1,8 @@
 package com.udea;
 
-import com.udea.classes.Casa;
-import com.udea.classes.Departamento;
-import com.udea.classes.LocalComercial;
-import com.udea.classes.Propiedad;
-
 public class Main {
     public static void main(String[] args) {
-        Propiedad[] propiedades = new Propiedad[3];
-        propiedades[0] = new LocalComercial("Calle 1 # 2-3", 100, true);
-        propiedades[1] = new Departamento("Calle 2 # 3-4", 200, 3, 2);
-        propiedades[2] = new Casa("Calle 3 # 4-5", 300, 50);
-
-        for (Propiedad propiedad : propiedades) {
-            System.out.println(propiedad.mostrarDescripcion());
-            System.out.println("Precio de alquiler: $" + propiedad.calcularPrecioAlquiler());
-            System.out.println();
-        }
+        String ANSI_GREEN = "\u001B[32m";
+        System.out.println(ANSI_GREEN + "Hola Técnicas de programación" + ANSI_GREEN);
     }
 }
--- polimorfismo/src/main/java/com/udea/interfaces/IMetodos.java
+++
@@ -1,7 +0,0 @@
-package com.udea.interfaces;
-
-public interface IMetodos {
-  public int calcularPrecioAlquiler();
-
-  public String mostrarDescripcion();
-}
