2b7e5a645eb624d4f76fd89eea9b13d04f61e016
Esteban <estebancg12@hotmail.com>
2024-10-17 09:49:32 -0500

    Cambiando el nombre a la carpeta 📁

--- poo/src/main/java/com/udea/Main.java
+++ poo/src/main/java/com/udea/Main.java
@@ -1,11 +1,12 @@
 package com.udea;
 
 public class Main {
-    static String ANSI_GREEN = "\u001B[32m";
+    static String ANSI_COLOR = "\u001B[35m";
 
     public static void main(String[] args) {
-        System.out.println(ANSI_GREEN);
+        System.out.println(ANSI_COLOR);
         System.out.println("Hola técnicas de programación");
-        System.out.println(ANSI_GREEN);
+        System.out.println(ANSI_COLOR);
+
     }
 }
