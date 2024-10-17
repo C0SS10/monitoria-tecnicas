5675fed00df284a58854776b8714b2b892def318
Esteban <estebancg12@hotmail.com>
2024-10-17 15:29:04 -0500

    Actualizando proyecto 🧰

--- poo/src/main/java/com/udea/Main.java
+++ poo/src/main/java/com/udea/Main.java
@@ -1,12 +1,11 @@
 package com.udea;
 
 public class Main {
-    static String ANSI_COLOR = "\u001B[35m";
+    static String ANSI_COLOR = "\u001B[36m";
 
     public static void main(String[] args) {
         System.out.println(ANSI_COLOR);
         System.out.println("Hola técnicas de programación");
         System.out.println(ANSI_COLOR);
-
     }
 }
